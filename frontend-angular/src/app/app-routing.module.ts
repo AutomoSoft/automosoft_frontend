9576cb53fc364c52a38ff8d41172482e3343a053
ShanManage <2017cs093@stu.ucsc.cmb.ac.lk>
2020-05-03 02:12:28 +0530

    change some components

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/Auth/login/login.component';
-import { HomeComponent } from './components/home/home.component';
 import { UserlandingComponent } from './components/userlanding/userlanding.component';
 import { RegisterUserComponent } from './components/Admin/register-user/register-user.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/main/home/home.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:"",component:HomeComponent},
   {path:"landing",component:UserlandingComponent},
-  {path:"registerUser",component:RegisterUserComponent}
+  {path:"registerUser",component:RegisterUserComponent},
+
+  {
+    path: "", component:MainComponent,
+    children: [
+      {path:'home', component:HomeComponent}
+    ]
+  }
 ];
 
 @NgModule({
