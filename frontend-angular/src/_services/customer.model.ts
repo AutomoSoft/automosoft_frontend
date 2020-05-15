9b80163a244f745486ae1451da8deb8ecfc68fba
lalinda <nawinjith96@gmail.com>
2020-05-16 01:58:17 +0530

    customer registration backend / dialog component added

--- frontend-angular/src/_services/customer.model.ts
+++
@@ -1,8 +0,0 @@
-export class Customer {
-    userid : Number;
-    firstName : string;
-    lastName : string;
-    email : string;
-    contactNo : Number;
-    vehicleRegNo : Number;
-}
--- frontend-angular/src/app/components/Admin/register-customer/register-customer.component.ts
+++ frontend-angular/src/app/components/Admin/register-customer/register-customer.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Customer } from '../../../../_services/customer.model';
+import { Router } from "@angular/router";
 
+import { MycookiesService } from "../../Admin/mycookies.service";
+import { MatDialog, MatSnackBar, MatSnackBarConfig } from "@angular/material";
+import { HttpClient } from "@angular/common/http";
+import {
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormGroup,
+  FormArray,
+  Form
+} from "@angular/forms";
+
+import { ConfirmationDialogComponent } from "../../Auth/confirmation-dialog/confirmation-dialog.component";
 
 @Component({
   selector: 'app-register-customer',
@@ -10,14 +22,81 @@ import { Customer } from '../../../../_services/customer.model';
 })
 export class RegisterCustomerComponent implements OnInit {
 
-  constructor() { }
+  cookie;
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private fb: FormBuilder,
+    private cookies: MycookiesService,
+    private dialog: MatDialog,
+  ) {
+    if (this.cookies.getCookie("userAuth") != "") {
+      this.cookie = JSON.parse(this.cookies.getCookie("userAuth"));
+    }
+}
+
+customerForm = this.fb.group({
+  userid: ["", Validators.required],
+  password: ["", Validators.required],
+  firstName: ["", Validators.required],
+  lastName: ["", Validators.required],
+  email: ["", Validators.required],
+  contactNo: ["", Validators.required],
+  vehicleRegNo: ["", Validators.required],
+
+});
+
+addCustomer() {
+  const registerCustomer = {
+    usertype : "Customer",
+    userid: this.customerForm.value.userid,
+    password: this.customerForm.value.password,
+    firstName: this.customerForm.value.firstName,
+    lastname: this.customerForm.value.lastname,
+    email: this.customerForm.value.email,
+    contactnumber: this.customerForm.value.contactNo,
+    vehicleRegNo: this.customerForm.value.vehicleRegNo,
+    // date: this.customerForm.value.date,
+  };
+
+  var url = "http://localhost:3000/users/register";
+
+  const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+    data: {
+      message: "Are you sure want to Add?",
+      buttonText: {
+        ok: "Yes",
+        cancel: "No"
+      }
+    }
+  });
+
+  dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+
+    if (confirmed) {
+      this.http.post<any>(url, registerCustomer).subscribe(res => {
+        if (res.state) {
+          console.log(res.msg);
+
+          // this.customerForm.reset();
+        } else {
+          console.log(res.msg);
+          alert("Error!! Try Again");
+          this.router.navigate([this.cookie.userid,'registerCustomer']);
+        }
+      });
+      console.log(registerCustomer);
+    }
+  });
+
+}
 
   ngOnInit() {
   }
 
 }
 /*.resetForm(form:NgForm)
-{ 
+{
   if(form != null)
 
  form.reset();
