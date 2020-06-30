98afba2ce8a04ea0526a3692db63c73fadeef574
lalinda <nawinjith96@gmail.com>
2020-06-30 19:44:41 +0530

    update: ongoing jobs

--- frontend-angular/src/app/components/Foreman/ongoing-jobs/ongoing-jobs.component.ts
+++ frontend-angular/src/app/components/Foreman/ongoing-jobs/ongoing-jobs.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { MycookiesService } from "../../Admin/mycookies.service";
+import { MatDialog, MatSnackBar, MatSnackBarConfig } from "@angular/material";
+import { HttpClient } from "@angular/common/http";
+
+import { ConfirmationDialogComponent } from "../../Auth/confirmation-dialog/confirmation-dialog.component";
 
 @Component({
   selector: 'app-ongoing-jobs',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OngoingJobsComponent implements OnInit {
 
-  constructor() { }
+  cookie;
+  currJobs;   //ongoing jobs
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private cookies: MycookiesService,
+    private dialog: MatDialog,
+    public snackBar: MatSnackBar,
+  ) {
+    if (this.cookies.getCookie("userAuth") != "") {
+      this.cookie = JSON.parse(this.cookies.getCookie("userAuth"));
+    }
+  }
 
   ngOnInit() {
+    var temp = this.cookies.getCookie("userAuth");
+    if(temp==""){
+      this.router.navigate(['/login']);
+    }
+
+// *************************************************** Get Ongoing Jobs ********************************************************
+
+      const url = "http://localhost:3000/jobs/getOngoingJobs";
+
+      this.http.get<any>(url).subscribe(res => {
+        if (res.state == false) {
+          let config = new MatSnackBarConfig();
+          config.duration = true ? 2000 : 0;
+          this.snackBar.open("Error Try Again !!! ", true ? "Retry" : undefined, config);
+        } else {
+
+          this.currJobs = res.data;
+          console.log(this.currJobs);
+
+        }
+      });
+
+
   }
 
 }
