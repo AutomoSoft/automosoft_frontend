ba25653c16a75c33ecfad7909ad95b90de9349ec
ishara lakshan <lakshan2015dk@gmail.com>
2020-07-01 22:46:14 +0530

    edit view-user component

--- frontend-angular/src/app/components/main/search-user/view-user/view-user.component.ts
+++ frontend-angular/src/app/components/main/search-user/view-user/view-user.component.ts
@@ -1,4 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { MycookiesService } from "../../../Admin/mycookies.service";
+import { MatDialog, MatSnackBar, MatSnackBarConfig } from "@angular/material";
+import { HttpClient } from "@angular/common/http";
+
+
+import { ConfirmationDialogComponent } from "../../../Auth/confirmation-dialog/confirmation-dialog.component";
+
+import {
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormGroup,
+  FormArray,
+  Form
+} from "@angular/forms";
+import { SearchUserComponent } from '../search-user.component';
+
+
+interface user {
+  _id: String;
+  usertype: String;
+  userid: String;
+  firstname: String;
+  lastname: String;
+  gender: String;
+  nicnumber: String;
+  address: String;
+  contactnumber: String;
+  email: String;
+  password: String;
+  addedon: String;
+  lastmodifiedon: String;
+  lastmodifiedby: String;
+  vehicles: [];
+
+  // filepath: String;
+}
 
 @Component({
   selector: 'app-view-user',
@@ -7,9 +45,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUserComponent implements OnInit {
 
-  constructor() { }
+  cookie;
 
-  ngOnInit() {
+  constructor(
+    private http: HttpClient,
+    private cookies: MycookiesService,
+    private router: Router,
+    public snackBar: MatSnackBar,
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+  ) {
+    this.cookie = JSON.parse(this.cookies.getCookie("userAuth"));
   }
 
+  ngOnInit() {
+
+
+  }
+
+  onEditUser() {
+    const dialogRef = this.dialog.open(SearchUserComponent, {
+      width: '640px'
+    });
+
+  }
 }
