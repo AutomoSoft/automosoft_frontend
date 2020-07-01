a46350a9a512607bb902bd3a18d055833ee91033
Sajith Thisera <thisera.sajith@gmail.com>
2020-07-02 00:10:34 +0530

    view-technicians added

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -75,6 +75,7 @@ import { OngoingJobsComponent } from './components/Foreman/ongoing-jobs/ongoing-
 
 import { MycookiesService } from './components/Admin/mycookies.service';
 import { ViewUserComponent } from './components/main/search-user/view-user/view-user.component';
+import { ViewTechniciansComponent } from './components/Foreman/view-technicians/view-technicians.component';
 
 @NgModule({
   declarations: [
@@ -107,6 +108,7 @@ import { ViewUserComponent } from './components/main/search-user/view-user/view-
     JobHistoryComponent,
     OngoingJobsComponent,
     ViewUserComponent,
+    ViewTechniciansComponent,
   ],
   imports: [
     BrowserModule,
