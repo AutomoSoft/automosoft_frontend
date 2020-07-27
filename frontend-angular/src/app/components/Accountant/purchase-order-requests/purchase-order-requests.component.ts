6d049197e0aa251673294a337dd2980af3f73996
Kavini <kavininanayakkara@gmail.com>
2020-07-28 01:05:07 +0530

    accountant-View PO requests

--- frontend-angular/src/app/components/Accountant/purchase-order-requests/purchase-order-requests.component.ts
+++ frontend-angular/src/app/components/Accountant/purchase-order-requests/purchase-order-requests.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { MycookiesService } from "../../Admin/mycookies.service";
+import { MatSnackBar, MatDialog, MatSnackBarConfig, MatTableDataSource, MatPaginator, MatSort, MatDialogConfig, MAT_DIALOG_DATA } from '@angular/material';
+import { HttpClient } from "@angular/common/http";
+import { ConfirmationDialogComponent } from "../../Auth/confirmation-dialog/confirmation-dialog.component";
 
 @Component({
   selector: 'app-purchase-order-requests',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseOrderRequestsComponent implements OnInit {
 
-  constructor() { }
+  approvedOrders;
+  cookie;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private cookies: MycookiesService,
+    private dialog: MatDialog,
+    public snackBar: MatSnackBar,
+  ) {
+    if (this.cookies.getCookie("userAuth") != "") {
+      this.cookie = JSON.parse(this.cookies.getCookie("userAuth"));
+    }
+  }
 
   ngOnInit() {
+
+    const url = "http://localhost:3000/purchaseOrders/fetchOrdersByStatus?status=1";
+
+    this.http.get<any>(url).subscribe(res => {
+      if (res.state == false) {
+        let config = new MatSnackBarConfig();
+        config.duration = true ? 2000 : 0;
+        this.snackBar.open("Error Try Again !!! ", true ? "Retry" : undefined, config);
+      } else {
+        this.approvedOrders = res.data;
+      }
+    });
+
+
   }
 
 }
