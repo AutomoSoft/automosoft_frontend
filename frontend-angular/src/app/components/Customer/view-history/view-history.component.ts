61e7fbbc4fe8ecfe6ccf4519dfac3428e0f73549
yasindi1996 <38649355+yasindi1996@users.noreply.github.com>
2020-07-30 14:35:48 +0530

    view-history content added

--- frontend-angular/src/app/components/Customer/view-history/view-history.component.ts
+++ frontend-angular/src/app/components/Customer/view-history/view-history.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MycookiesService } from '../../Admin/mycookies.service';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatSnackBar, MatSnackBarConfig } from '@angular/material';
+
+interface user {
+  _id: String;
+  usertype: String;
+  userid: String;
+  vehicles: String;
+  filepath: String;
+}
+
+interface job {
+  _id: String;
+  jobNo: String;
+  jobType: String;
+  custId: String;
+  jobStatus: String;
+  technicians: String;
+  itemsUsed: String;
+}
 
 @Component({
   selector: 'app-view-history',
   templateUrl: './view-history.component.html',
   styleUrls: ['./view-history.component.scss']
 })
+
+
 export class ViewHistoryComponent implements OnInit {
 
-  constructor() { }
+  cookie;
+  jobHis;   //job history
+  userdata: user[] = [];
+  job: job[] = [];
+  userid;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private cookies: MycookiesService,
+    private dialog: MatDialog,
+    public snackBar: MatSnackBar,
+  ) {
+    if (this.cookies.getCookie("userAuth") != "") {
+      this.cookie = JSON.parse(this.cookies.getCookie("userAuth"));
+    }
+   }
 
   ngOnInit() {
+    var temp = this.cookies.getCookie("userAuth");
+    if (temp == "") {
+      this.router.navigate(['/login']);
+    }
+
+
+    const url = "http://localhost:3000/jobs/viewServices";
+
+    this.http.get<any>(url + "/" + this.cookie.userid).subscribe(res => {
+      if (res.state == false) {
+        let config = new MatSnackBarConfig();
+        config.duration = true ? 2000 : 0;
+        this.snackBar.open("Error Try Again !!! ", true ? "Retry" : undefined, config);
+      } else {
+
+        this.jobHis = res.data;
+        console.log(this.jobHis);
+        console.log()
+
+      }
+    });
   }
 
 }
+//
