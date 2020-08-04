3fc0c71d4255944a35f1289e166cc97eca4e8391
Kavini <kavininanayakkara@gmail.com>
2020-08-04 09:43:59 +0530

    new PO component moved

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -97,7 +97,6 @@ const routes: Routes = [
   /****************************************** Accountant Routes ******************************************** */
   { path: "createInvoice", component: CreateInvoiceComponent },
   { path: "invoiceHistory", component: InvoiceHistoryComponent },
-  { path: "newPurchaseOrder", component: NewPurchaseOrderComponent },
   { path: "purchaseOrderRequests", component: PurchaseOrderRequestsComponent },
 
 ];
