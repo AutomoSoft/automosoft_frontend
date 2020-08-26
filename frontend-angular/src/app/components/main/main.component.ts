4326a018adc52a299a5a3566020e93b9f44a159c
ishara lakshan <lakshan2015dk@gmail.com>
2020-08-26 23:28:35 +0530

    finished: Admin sidebar

--- frontend-angular/src/app/components/main/main.component.ts
+++ frontend-angular/src/app/components/main/main.component.ts
@@ -1,4 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MycookiesService } from '../Admin/mycookies.service';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+
+interface user {
+  _id: String;
+  usertype: String;
+  userid: String;
+  firstname: String;
+  lastname: String;
+  gender: String;
+  nicnumber: String;
+  address: String;
+  contactnumber: String;
+  email: String;
+  password: String;
+  addedon: String;
+  lastmodifiedon: String;
+  lastmodifiedby: String;
+  vehicles: String;
+
+}
 
 @Component({
   selector: 'app-main',
@@ -7,14 +30,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  collapsed=true;
-  constructor() { }
+  collapsed = true;
+  userdata: user[] = [];
+  cookie;
+  userid;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private cookies: MycookiesService,
+    private http: HttpClient,
+    public snackBar: MatSnackBar,
+  ) {
+    this.cookie = JSON.parse(this.cookies.getCookie("userAuth"));
   }
 
-  toggle(){
-    this.collapsed=!this.collapsed;
+
+  ngOnInit() {
+    var temp = this.cookies.getCookie("userAuth");
+    if (temp == "") {
+      this.router.navigate(['/login']);
+    } else {
+      this.userid = this.cookie.userid;
+
+      const url = "http://localhost:3000/users/searchUsers"   //backend url
+
+      this.http.get<any>(url + "/" + this.userid).subscribe(res => {
+        if (res.state == false) {
+          let config = new MatSnackBarConfig();
+          config.duration = true ? 2000 : 0;
+          this.snackBar.open("No User Found..! ", true ? "Retry" : undefined, config);
+        } else {
+          this.userdata = res.data;
+
+        }
+      });
+    }
+  }
+
+  toggle() {
+    this.collapsed = !this.collapsed;
   }
 
 
